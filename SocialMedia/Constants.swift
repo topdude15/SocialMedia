e1bd7d501a6eb18b35213da9e18185f74808ce78
topdude15 <topdude15@gmail.com>
2017-03-12 15:12:16 -0400

    Updated sign-in UI with custom classes

---
+++ SocialMedia/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  SocialMedia
+//
+//  Created by Trevor Rose on 3/12/17.
+//  Copyright © 2017 Trevor Rose. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
