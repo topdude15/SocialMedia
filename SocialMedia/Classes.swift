e4fef079e8c917ddcdc505b5ff10754e6c7a4367
topdude15 <topdude15@gmail.com>
2017-03-29 12:44:47 -0400

    New screen sizes supported.  Usernames functional

---
+++ SocialMedia/Classes.swift
@@ -0,0 +1,9 @@
+//
+//  Classes.swift
+//  SocialMedia
+//
+//  Created by Trevor Rose on 3/22/17.
+//  Copyright © 2017 Trevor Rose. All rights reserved.
+//
+
+import Foundation
--- SocialMedia/UsernameVC.swift
+++ SocialMedia/UsernameVC.swift
@@ -2,17 +2,14 @@
 //  usernameVC.swift
 //  SocialMedia
 //
-//  Created by Trevor Rose on 3/21/17.
+//  Created by Trevor Rose on 3/22/17.
 //  Copyright © 2017 Trevor Rose. All rights reserved.
 //
 
 import UIKit
-import Firebase
-import SwiftKeychainWrapper
 
 class usernameVC: UIViewController {
 
-    @IBOutlet weak var usernameBox: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -23,23 +20,16 @@ class usernameVC: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-//    @IBAction func continueTapped(_ sender: Any) {
-//        let usernameValue = usernameBox.text //Gets value from username box
-//        let username = ["username": usernameValue]   //Sets dictonary for username to be set into Firebase database
-//        //FIRAuth.auth()?.signIn(withEmail: email, password: password, completion: { (user, error) in     //Logs the user into Firebase based on username and password set in the sign in VC
-//            //DataService.ds.addFirebaseUsername(uid: (user?.uid)!, username: username as! Dictionary<String, String>)   //Adds the username to the database
-//            self.performSegue(withIdentifier: "feedFromUsername", sender: nil)
-//        }
-  
-    @IBAction func addUsername(_ sender: Any) {
-        let usernameValue = usernameBox.text //Gets value from username box
-        let username = ["username": usernameValue]   //Sets dictonary for username to be set into Firebase database
-        //FIRAuth.auth()?.signIn(withEmail: email, password: password, completion: { (user, error) in     //Logs the user into Firebase based on username and password set in the sign in VC
-        //DataService.ds.addFirebaseUsername(uid: (user?.uid)!, username: username as! Dictionary<String, String>)   //Adds the username to the database
-        self.performSegue(withIdentifier: "feedFromUsername", sender: nil)
-    }
     
-//    )
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
 }
-//}
